1c16f7c73185a91b98850586ddb5ab2dddfec239
Melissa Manning <melissa.manning3@gmail.com>
2015-07-28 14:43:04 -0400

    added hover

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+   $("div.centerphoto").mouseenter(function() {
+       $("div.next").show();
+     $("div.centerphoto").mouseleave(function() {
+           $("div.next").hide();
+   });
+});
+});
