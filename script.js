eab0e240a253453c866e80c82f425bec91b82608
Melissa Manning <melissa.manning3@gmail.com>
2015-07-29 15:39:32 -0400

    trim query fat

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
     $("img").mouseenter(function() {
-        $("div.next").show();
+        $("div.next, div.previous, div.daycount").show();
     });
      $("img").mouseleave(function() {
-         $("div.next").hide();
-    });
-     $("img").mouseenter(function() {
-         $("div.previous").show();
-    });
-     $("img").mouseleave(function() {
-         $("div.previous").hide();
-    });
-     $("img").mouseenter(function() {
-         $("div.daycount").show();
-    });
-     $("img").mouseleave(function() {
-         $("div.daycount").hide();
+         $("div.next, div.previous, div.daycount").hide();
     });
      $("div.moreinfo").click(function() {
          $("div.panel").slideToggle('slow');
