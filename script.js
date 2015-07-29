70623441c3b4916dfdd8c471b712b3dcb3ca1f4e
Melissa Manning <melissa.manning3@gmail.com>
2015-07-29 15:18:11 -0400

    Add toggle plus/minue

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
     $("img").mouseenter(function() {
         $("div.next").show();
-        });
+    });
      $("img").mouseleave(function() {
          $("div.next").hide();
-         });
+    });
      $("img").mouseenter(function() {
          $("div.previous").show();
-         });
+    });
      $("img").mouseleave(function() {
          $("div.previous").hide();
-        });
+    });
      $("img").mouseenter(function() {
          $("div.daycount").show();
-        });
+    });
      $("img").mouseleave(function() {
          $("div.daycount").hide();
-         });
+    });
      $("div.moreinfo").click(function() {
-       $("div.panel").slideToggle('slow');
-
-     });
+         $("div.panel").slideToggle('slow');
+         if ($(this).text() === "+") {
+           $(this).text('-');
+         }
+         else {
+           $(this).text('+');
+         }
+    });
 
  });
