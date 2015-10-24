29f36eb977e9e65bc9489c02e309c12e391c0d43
Melissa Manning <melissa.manning3@gmail.com>
2015-10-23 22:16:29 -0400

    right hover add
    add hover overs for bottom right

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-    $("img, img.next, img.previous, div.daycount, div.location").mouseenter(function() {
+    $("img.main, img.next, img.previous, div.daycount, div.location").mouseenter(function() {
         $("img.next, img.previous, div.daycount, div.location").show();
     });
-     $("img").mouseleave(function() {
+     $("img.main").mouseleave(function() {
          $("img.next, img.previous, div.daycount, div.location").hide();
     });
-     $("div.moreinfo").click(function() {
-         $("div.panel").slideToggle('slow');
-         if ($(this).text() === "+") {
-           $(this).text('-');
-         }
-         else {
-           $(this).text('+');
-         }
+
+    $("img.bottomright, div.bottomrightdescription").mouseenter(function() {
+        $("div.bottomrightdescription").show();
     });
+     $("img.bottomright").mouseleave(function() {
+         $("div.bottomrightdescription").hide();
+    });
+
 
  });
