856cbe3404bfe358fd19b9426eb518a001f8802d
Melissa Manning <melissa.manning3@gmail.com>
2015-10-24 17:46:47 -0400

    added transition to descriptions/navigation

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     $("img.main, img.next, img.previous, div.daycount, div.location").mouseenter(function() {
-        $("img.next, img.previous, div.daycount, div.location").show();
+        $("img.next, img.previous, div.daycount, div.location").addClass("active");
     });
      $("img.main").mouseleave(function() {
-         $("img.next, img.previous, div.daycount, div.location").hide();
+         $("img.next, img.previous, div.daycount, div.location").removeClass("active");
     });
 
     $("img.bottomright, div.bottomrightdescription").mouseenter(function() {
-        $("div.bottomrightdescription").show();
+        $("div.bottomrightdescription").addClass("active");
     });
      $("img.bottomright").mouseleave(function() {
-         $("div.bottomrightdescription").hide();
+         $("div.bottomrightdescription").removeClass("active");
     });
 
 
